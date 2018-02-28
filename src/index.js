7566b626b2467b27d82e927b147fd258cb31d880
Nikitos09 <nikita0999.99@mail.ru>
2018-02-28 23:07:41 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   str = str.split('');
+   var open = 0;
+   var close = 0;
+   var openclose = 0;
+   for (var i=0; i<str.length; i++ ) {
+      if (str[i]==('['|| '(' || '1' || '3' || '5' ||'{')) {
+        open++
+      }
+      if (str[i]==(']'|| ')' || '2' || '4' || '6' || '}')) {
+        close++
+      }
+      if (str[i]==('7'|| '8' || '|' || '' )) {
+        openclose++
+      }
+   }
+
+   if ((open == close) && (openclose % 2 == 0) && (str.length % 2 == 0)) {
+     return true;
+   }
+   else {
+     return false;
+   }
+   }
